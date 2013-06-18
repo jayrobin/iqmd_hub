194367149f63040ea47744a24f987302491aec5c
jayrobin <james.michael.robinson@googlemail.com>
2013-06-18 11:56:05 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameHub::Application.config.secret_token = 'be56b977bd71446d49ea6fc5bfa6191842c37d2e4736f6fe0cfd9b203821e733752245bf036d9741cf86eb7c40b2dd67d83418bf4e46adb3562f125c0c679fee'
